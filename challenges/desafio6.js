8b5e1424f3978be4251ea84d52045181edceb851
Gustavo Pospi <gutxander@gmail.com>
2022-09-30 05:48:58 -0300

    feat: muda requisito 6.

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -1,9 +1,4 @@
-db.produtos.find({
-  $and: [
-    { curtidas: { $gt: 10 } },
-    { curtidas: { $lt: 100 } },
-  ],
-}, {
+db.produtos.find({ curtidas: { $gt: 10, $lt: 100 } }, {
   name: true,
   curtidas: true,
   _id: false,
